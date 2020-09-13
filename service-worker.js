ced79850529332033264f8429fe8368c60d6c9ee
Kieran Warwick <kwarwick97@gmail.com>
2020-09-13 12:09:19 +0100

    more reactivety

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b5e4281685c0e0dccdd1b1a04597f191.js"
+  "/precache-manifest.4244d424c448c883e09c4bf10d0f92b3.js"
 );
 
 self.addEventListener('message', (event) => {
